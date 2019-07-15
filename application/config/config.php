9effa0993f8587a3f56792fc0eead449b0c5bd73
Faisal Ramadhan Budiono <ucul.project@gmail.com>
2019-07-16 01:33:20 +0700

    Edit config base_url
    Adding .htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://break-free.cul';
+$config['base_url'] = 'http://www.break-free.asia';
 
 /*
 |--------------------------------------------------------------------------
